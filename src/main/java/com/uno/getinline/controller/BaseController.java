6a5023b8e314750060f189c0ec64f3ef7338ef2f
Grandes <ho2708@naver.com>
2022-05-22 07:46:35 +0900

    feat(clone coding): CH02. API 설계 - 1강

--- src/main/java/com/uno/getinline/controller/BaseController.java
+++ src/main/java/com/uno/getinline/controller/BaseController.java
@@ -3,6 +3,7 @@ package com.uno.getinline.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BaseController implements ErrorController {
@@ -13,7 +14,7 @@ public class BaseController implements ErrorController {
     }
 
 
-    @GetMapping("/error")
+    @RequestMapping("/error")
     public String error() {
         return "error";
     }
